cdfbb5ac6f056fd2d1940d5cd9c63e3f35ce3f0f
Mohamed Gamal <mohamedgamalfahmy1@gamil.com>
2025-10-01 04:29:53 +0300

    project ready for deploy

--- cart/vite.config.js
+++ cart/vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Tailwind v4 plugin
-  ],
-})
+  plugins: [react()],
+});
